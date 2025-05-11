ccb9274cd34e6374510814a81404f7d04bc115e0
Oussema Dhaouadi <oussema@deepscenario.com>
2025-05-11 16:47:10 +0200

    fixing imports

--- ortholoc/correspondences/__init__.py
+++ ortholoc/correspondences/__init__.py
@@ -1,4 +1,4 @@
-from .correspondences import Correspondences
+from .Correspondences import Correspondences
 from .Correspondences2D2D import Correspondences2D2D
 from .Correspondences2D3D import Correspondences2D3D
 __all__ = ["Correspondences", "Correspondences2D2D", "Correspondences2D3D"]
